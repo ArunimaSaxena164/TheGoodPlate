b5d04824eed6504b79f57ec0128f2a8d8e2b2e5e
Arunima Saxena <arunima16042005@gmail.com>
2025-11-01 20:22:06 +0530

    some changes

---
+++ frontend/src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Feature from './Feature';
+import Hero from './Hero';
+import Impact from './Impact';
+function Home() {
+    return ( 
+       <div>
+        <Hero/>
+        <Feature/>
+        <Impact/>
+       </div>
+     );
+}
+
+export default Home;
---
+++ frontend/src/Home/Impact.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Impact() {
+    return (  
+        <>Impact</>
+    );
+}
+
+export default Impact;
